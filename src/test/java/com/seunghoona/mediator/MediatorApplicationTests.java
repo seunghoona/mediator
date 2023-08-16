f253bfe815784114dec0379831b9314792c84d86
seunghoona <naseunghoo@naver.com>
2023-08-17 07:06:08 +0900

    init

---
+++ src/test/java/com/seunghoona/mediator/MediatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seunghoona.mediator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
