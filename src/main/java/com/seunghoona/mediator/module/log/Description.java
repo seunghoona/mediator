6a6c9e7e782565306dcb3fe04917937ba07cd4d1
seunghoona <naseunghoo@naver.com>
2023-09-05 14:01:33 +0900

    feat : API 호출 출력 로그 어노테이션 생성

---
+++ src/main/java/com/seunghoona/mediator/module/log/Description.java
@@ -0,0 +1,14 @@
+package com.seunghoona.mediator.module.log;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface Description {
+	String value();
+}
